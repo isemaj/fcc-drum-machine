47d6c748c57d6ca2d650d4125f30dadeaf864f45
James Itum <jamesitum@protonmail.ch>
2018-08-28 22:48:11 +0800

    update

--- src/js/containers/DrumPad.js
+++ src/js/containers/DrumPad.js
@@ -2,103 +2,108 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import {
-  pressQ,
-  pressW,
-  pressE,
-  pressA,
-  pressS,
-  pressD,
-  pressZ,
-  pressX,
-  pressC,
-} from '../actions/index';
-
 const styles = {
   height: 30,
   width: 30,
   border: '1px solid black',
 };
 
-const play = (key) => {
-  if(document.readyState === 'complete') {
-    console.log(key);
-    const sound = document.getElementById(key);
-    sound.play();
-  }
-};
+// const play = (key) => {
+//   if(document.readyState === 'complete') {
+//     console.log(key);
+//     const sound = document.getElementById(key);
+//     sound.play();
+//   }
+// };
 
 
 class DrumPad extends Component {
   constructor(props) {
     super(props);
-    this.keyDown = this.keyDown.bind(this); 
   }
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.keyDown);
+  // componentDidMount() {
+  //   document.addEventListener('keydown', this.keyDown);
+  // }
+
+  // componentWillUnmount() {
+  //   document.removeEventListener('keydown', this.keyDown);
+  // }
+
+  // keyDown = (e) => {
+  //   if(e.keyCode === 80) {
+  //     play()
+  //   }
+  // }
+  // trigger(index, key) {
+  // 	switch (index) {
+  //     case 0:
+  //       play(key);
+  // 			return this.props.pressQ;
+  //       break;
+  // 		case 1:
+  //       play(key);
+  // 			return this.props.pressW;
+  // 		case 2:
+  // 			return this.props.pressE;
+  // 		case 3:
+  // 			return this.props.pressA;
+  // 		case 4:
+  // 			return this.props.pressS;
+  // 		case 5:
+  // 			return this.props.pressD;
+  // 		case 6:
+  // 			return this.props.pressZ;
+  // 		case 7:
+  // 			return this.props.pressX;
+  // 		case 8:
+  // 			return this.props.pressC;
+  // 		default:
+  // 	}
+  // }
+
+  // onePad() {
+  //   return this.props.pad.map((elem, index) => (
+  //     <div
+  //       key={elem.id}
+  //       style={styles}
+  //       className="drum-pad"
+  //       onClick={this.trigger(index, elem.keyLetter)}
+  //       // onClick={() => play(elem.keyLetter)}
+  //     >
+  //       {elem.keyLetter}
+  //       <audio
+  //         className="clip"
+  //         id={elem.keyLetter}
+  //         src={elem.url}
+  //       />
+  //     </div>
+  //   ));
+  // }
+
+  playNow = () => {
+   const sound = this.refs.Q; 
+   sound.play(); 
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyDown);
-  }
-
-  keyDown(e) {
-    if(e.keyCode === 80) {
-      play()
-    }
-  }
-  trigger(index, key) {
-  	switch (index) {
-      case 0:
-        play(key);
-  			return this.props.pressQ;
-        break;
-  		case 1:
-        play(key);
-  			return this.props.pressW;
-  		case 2:
-  			return this.props.pressE;
-  		case 3:
-  			return this.props.pressA;
-  		case 4:
-  			return this.props.pressS;
-  		case 5:
-  			return this.props.pressD;
-  		case 6:
-  			return this.props.pressZ;
-  		case 7:
-  			return this.props.pressX;
-  		case 8:
-  			return this.props.pressC;
-  		default:
-  	}
-  }
-
-  onePad() {
-    return this.props.pad.map((elem, index) => (
+  render() {
+    const padSet = this.props.kit.drumKit.map((elem, index) => (
       <div
         key={elem.id}
         style={styles}
-        className="drum-pad"
-        onClick={this.trigger(index, elem.keyLetter)}
-        // onClick={() => play(elem.keyLetter)}
+        onClick={this.playNow}
       >
-        {elem.keyLetter}
+        {elem.keyTrigger}
         <audio
           className="clip"
-          id={elem.keyLetter}
+          ref={elem.keyTrigger}
           src={elem.url}
         />
       </div>
     ));
-  }
-
-
-  render() {
     return (
       <div>
-        {this.onePad()}
+        {padSet}
       </div>
     );
   }
@@ -107,18 +112,7 @@ class DrumPad extends Component {
 
 const mapStateToProps = state => ({
   pad: state.pad,
+  kit: state.kit,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  pressQ,
-  pressW,
-  pressE,
-  pressA,
-  pressS,
-  pressD,
-  pressZ,
-  pressX,
-  pressC,
-}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(DrumPad);
+export default connect(mapStateToProps, null)(DrumPad);
