e6d602dcbf8615b0404dc96cfc12fe11df6866c6
James Itum <jamesitum@protonmail.ch>
2018-08-28 17:36:04 +0800

    use arrow function class property

--- src/js/containers/Kit.js
+++ src/js/containers/Kit.js
@@ -7,26 +7,17 @@ import { selectKit } from '../actions/index';
 class Kit extends Component {
   constructor(props) {
     super(props);
-    this.validateCheck = this.validateCheck.bind(this);
   }
 
-  validateCheck() {
-  	const click = document.getElementById('kit-selector');
-  	click.addEventListener('click', function () {
-  		console.log(this.checked);
-  		this.props.selectKit(this.checked);
-  	// 	if (this.checked) {
-  	// 		this.props.selectKit(this.checked);
-  	// 	} else {
-  	// 		this.props.selectKit(this.checked);
-  	// 	}
-    });
-  }
+  validateCheck = () => {
+  console.log(this.checked);
+  this.props.selectKit(this.checked);
+}
 
   render() {
     return (
       <React.Fragment>
-        <input id="kit-selector" type="checkbox" onChange={this.validateCheck} />
+        <input id="kit-selector" type="checkbox" onClick={this.validateCheck} />
       </React.Fragment>
     );
   }
