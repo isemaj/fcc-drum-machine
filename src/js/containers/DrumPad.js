3574f36125b3ea56b18f90f4dec8b83174e49c24
James Itum <jamesitum@protonmail.ch>
2018-09-03 23:35:46 +0800

    update

--- src/js/containers/DrumPad.js
+++ src/js/containers/DrumPad.js
@@ -5,80 +5,18 @@ import { connect } from 'react-redux';
 import { displayNow } from '../actions/index';
 import SinglePad from './SinglePad';
 
-const inactivePad = {
-  height: 90,
-  width: 90,
-  color: 'var(--text-inactive-and-hover)',
-  borderRadius: 5,
-  backgroundColor: 'white',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  animation: 'flick 1s infinite alternate'
-};
-
-const activePad = {
-  height: 90,
-  width: 90,
-  color: 'var(--text-active)',
-  borderRadius: 5,
-  backgroundColor: 'white',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  boxShadow: '0px 0px 25px var(--shadow-box), inset 0px 0px 14px var(--shadow-box)',
-};
-
 class DrumPad extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squareStyle: inactivePad 
-    }
-    this.activate = this.activate.bind(this);
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.keyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyPress);
-  }
-
-  keyPress = (e) => {
-    this.playNow(e.key.toUpperCase());
-  }
-
-  activate = () => {
-    if (this.props.power.powerStatus) {
-      this.setState({
-        squareStyle: activePad
-      });
-    }
-  }
-
-  playNow = (key, id) => {
-    const sound = document.getElementById(key);
-    const keyIndex = this.props.kit.drumKit.findIndex(elem => elem.keyTrigger === key)
-    if(keyIndex === -1) {
-      return
-    } else { 
-      const keyId = this.props.kit.drumKit[keyIndex].id;
-      this.props.displayNow(keyId);
-      sound.volume = this.props.volume.currentVolume;
-      sound.play();
-      this.activate();
-    }
   }
 
   render() {
-    const padSet = this.props.kit.drumKit.map((elem, index) => (
-        <SinglePad key={elem.id} elemId={elem.id} source={elem.url}/>
-    ))
+    const padSet = this.props.kit.drumKit.map((elem, index, currarr) => (
+      <SinglePad keyCode={elem.keyCode} key={elem.id} drumId={currarr[index].id} elemKey={elem.keyTrigger} source={elem.url} />
+    ));
     return (
       <React.Fragment>
-        {padSet} 
+        {padSet}
       </React.Fragment>
     );
   }
@@ -86,8 +24,6 @@ class DrumPad extends Component {
 
 const mapStateToProps = state => ({
   kit: state.kit,
-  power: state.power, 
-  volume: state.volume,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
--- src/js/containers/SinglePad.js
+++ src/js/containers/SinglePad.js
@@ -4,26 +4,96 @@ import { connect } from 'react-redux';
 
 import { displayNow } from '../actions/index';
 
-class SinglePad extends Component {
-	constructor(props) {
-		super(props);
+const inactivePad = {
+  height: 90,
+  width: 90,
+  color: 'var(--text-inactive-and-hover)',
+  borderRadius: 5,
+  backgroundColor: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  animation: 'flick 1s infinite alternate'
+};
 
-	}
-	render() {
-		return (
-			<div> 
-				<audio>{this.props.elemId}</audio>
-			</div>
-		)
-	}
+const activePad = {
+  height: 90,
+  width: 90,
+  color: 'var(--text-active)',
+  borderRadius: 5,
+  backgroundColor: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  boxShadow: '0px 0px 25px var(--shadow-box), inset 0px 0px 14px var(--shadow-box)',
+};
+
+class SinglePad extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	currentStyle: inactivePad 
+    }
+  }
+
+  componentDidMount() {
+  	document.addEventListener('keydown', this.keyPress);
+  	console.log(this.props.drumId);
+  }
+
+  componentWillUnmount() {
+  	document.removeEventListener('keydown', this.keyPress);
+  }
+
+  keyPress = (e) => {
+  	if (e.keyCode === this.props.keyCode) {
+  	this.playNow(e.key.toUpperCase());
+  	}
+  }
+
+  activate = () => {
+  	if (this.props.power.powerStatus) {
+  		this.state.currentStyle == activePad ? 
+  			this.setState({
+  				currentStyle: inactivePad
+  			})
+  			:
+  			this.setState({
+  				currentStyle: activePad
+  			})
+  	}
+  }
+
+  playNow = () => {
+  	const sound = document.getElementById(this.props.elemKey);
+  	sound.currentTime = 0;
+  	sound.volume = this.props.volume.currentVolume;
+  	sound.play();
+  	this.activate();
+  	setTimeout(() => this.activate(), 100);
+  	this.props.displayNow(this.props.drumId);
+  }
+
+  render() {
+  	const source = this.props.source;
+  	const trigger =  this.props.elemKey;
+  	const drumpadId = this.props.drumId;
+    return (
+      <div style={this.state.currentStyle} onClick={() => this.playNow()}>
+      	{trigger} 
+        <audio id={trigger} src={this.props.power.powerStatus ? source : '#'} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-	power: state.power,
-})
+  power: state.power,
+  volume: state.volume,
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-	displayNow,
+  displayNow,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(SinglePad);
