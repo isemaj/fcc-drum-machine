28a0acf74c8b63a74d4aea56a7d99bee73fc1f89
James Itum <jamesitum@protonmail.ch>
2018-09-05 12:36:25 +0800

    udpates

--- src/js/containers/SwitchButton.js
+++ src/js/containers/SwitchButton.js
@@ -6,13 +6,70 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { turnOn, turnOff } from '../actions/index';
 
+
+const powerOnStyle = {
+  backgroundColor: 'var(--circ-back)',
+  outline: 'none',
+  border: 'none',
+  width: 80,
+  height: 87,
+  borderRadius: '50%',
+  justifySelf: 'start',
+  marginLeft: 30,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: 'inset 0px 2px 8px var(--circ-back-shadow)',
+};
+
+const powerOffStyle = {
+  backgroundColor: 'var(--circ-back)',
+  outline: 'none',
+  border: 'none',
+  width: 80,
+  height: 87,
+  borderRadius: '50%',
+  justifySelf: 'start',
+  marginLeft: 30,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: 'inset 0px 2px 8px var(--circ-back-shadow)',
+};
+
+const circOn = {
+  backgroundColor: 'var(--circ-front)',
+  outline: 'none',
+  width: 73,
+  height: 73,
+  borderRadius: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: '0px 5px 4px var(--circ-front-shadow-dark)',
+};
+
+const circOff = {
+  backgroundColor: 'var(--circ-front)',
+  outline: 'none',
+  width: 73,
+  height: 73,
+  borderRadius: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: 'inset 0px 0.5px 2px var(--circ-front-shadow), 0px 2px 3px var(--circ-front-shadow-dark)',
+};
+
 const SwitchButton = props => (
   <React.Fragment>
-    <button className="switch-selector" type="submit" onClick={!props.power.powerStatus ? props.turnOn : props.turnOff}>
-      {props.power.powerStatus
-	    ? <FontAwesomeIcon icon="lightbulb" size="3x" color="white" />
-      	: <FontAwesomeIcon icon="lightbulb" size="3x" color="yellow" />
+    <button style={props.power.powerStatus ? powerOnStyle : powerOffStyle} type="submit" onClick={!props.power.powerStatus ? props.turnOn : props.turnOff}>
+      <div style={props.power.powerStatus ? circOn : circOff}>
+        {props.power.powerStatus
+	    ? <FontAwesomeIcon icon="power-off" size="3x" color="#FF0492" />
+      	: <FontAwesomeIcon icon="power-off" size="3x" color="#240B2D" />
       }
+      </div>
     </button>
   </React.Fragment>
 );
--- src/js/reducers/kitReducer.js
+++ src/js/reducers/kitReducer.js
@@ -3,22 +3,22 @@ import { KIT_SELECT } from '../constants/ActionTypes';
 const firstDrumSet = [{
   keyCode: 81,
   keyTrigger: 'Q',
-  id: 'Heater-1',
+  id: 'Heater 1',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
 }, {
   keyCode: 87,
   keyTrigger: 'W',
-  id: 'Heater-2',
+  id: 'Heater 2',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
 }, {
   keyCode: 69,
   keyTrigger: 'E',
-  id: 'Heater-3',
+  id: 'Heater 3',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
 }, {
   keyCode: 65,
   keyTrigger: 'A',
-  id: 'Heater-4',
+  id: 'Heater 4',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
 }, {
   keyCode: 83,
@@ -28,12 +28,12 @@ const firstDrumSet = [{
 }, {
   keyCode: 68,
   keyTrigger: 'D',
-  id: 'Open-HH',
+  id: 'Open HH',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
 }, {
   keyCode: 90,
   keyTrigger: 'Z',
-  id: "Kick-n'-Hat",
+  id: "Kick n' Hat",
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
 }, {
   keyCode: 88,
@@ -43,7 +43,7 @@ const firstDrumSet = [{
 }, {
   keyCode: 67,
   keyTrigger: 'C',
-  id: 'Closed-HH',
+  id: 'Closed HH',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
 },
 ];
@@ -51,17 +51,17 @@ const firstDrumSet = [{
 const secondDrumSet = [{
   keyCode: 81,
   keyTrigger: 'Q',
-  id: 'Chord-1',
+  id: 'Chord 1',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
 }, {
   keyCode: 87,
   keyTrigger: 'W',
-  id: 'Chord-2',
+  id: 'Chord 2',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
 }, {
   keyCode: 69,
   keyTrigger: 'E',
-  id: 'Chord-3',
+  id: 'Chord 3',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
 }, {
   keyCode: 65,
@@ -71,22 +71,22 @@ const secondDrumSet = [{
 }, {
   keyCode: 83,
   keyTrigger: 'S',
-  id: 'Open-HH',
+  id: 'Open HH',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
 }, {
   keyCode: 68,
   keyTrigger: 'D',
-  id: 'Closed-HH',
+  id: 'Closed HH',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
 }, {
   keyCode: 90,
   keyTrigger: 'Z',
-  id: 'Punchy-Kick',
+  id: 'Punchy Kick',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
 }, {
   keyCode: 88,
   keyTrigger: 'X',
-  id: 'Side-Stick',
+  id: 'Side Stick',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
 }, {
   keyCode: 67,
